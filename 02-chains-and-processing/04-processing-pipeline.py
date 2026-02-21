0fae009fb79b14d9e3331196c5950dd17e42af7a
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2026-02-21 16:41:39 -0300

    feat: :sparkles: starting with pipelines

---
+++ 02-chains-and-processing/04-processing-pipeline.py
@@ -0,0 +1,32 @@
+from dotenv import load_dotenv
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv()
+
+llm_en = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0,
+)
+
+template_translate = PromptTemplate(
+    input_variables=["initial_text"],
+    template="Translate the following text to English:\n ```{initial_text}```",
+)
+
+template_summary = PromptTemplate(
+    input_variables=["text"],
+    template="Summarize the following text in 4 words:\n ```{text}```",
+)
+
+translate = template_translate | llm_en | StrOutputParser()
+pipeline = {"text": translate} | template_summary | llm_en | StrOutputParser()
+
+result = pipeline.invoke(
+    {
+        "initial_text": "LangChain é um framework para "
+        "desenvolvimento de aplicações de IA"
+    }
+)
+print(result)
