93dc61260105520d2154e77c7c1e327548e95d7b
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2026-02-22 16:57:26 -0300

    feat: :sparkles: react agent and tools development

--- ch03_agents_and_tools/p01_react_agent_and_tools.py
+++ ch03_agents_and_tools/p01_react_agent_and_tools.py
@@ -1,5 +1,8 @@
 from dotenv import load_dotenv
 from langchain.tools import tool
+from langchain_classic.agents import AgentExecutor, create_react_agent
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
@@ -38,3 +41,60 @@ def web_search_mock(query: str) -> str:
             return f"The capital of {country} is {capital}."
 
     return "I don't know the capital of that country."
+
+
+llm = ChatOpenAI(
+    model="gpt-5-mini",
+    disable_streaming=True,
+)
+tools = [calculator, web_search_mock]
+
+prompt = PromptTemplate.from_template(
+    """
+Answer the following questions as best you can. You have access to the following tools.
+Only use the information you get from the tools, even if you know the answer.
+If the information is not provided by the tools, say you don't know.
+
+{tools}
+
+Use the following format:
+
+Question: the input question you must answer
+Thought: you should always think about what to do
+Action: the action to take, should be one of [{tool_names}]
+Action Input: the input to the action
+Observation: the result of the action
+
+... (this Thought/Action/Action Input/Observation can repeat N times)
+Thought: I now know the final answer
+Final Answer: the final answer to the original input question
+
+Rules:
+- If you choose an Action, do NOT include Final Answer in the same step.
+- After Action and Action Input, stop and wait for Observation.
+- Never search the internet. Only use the tools provided.
+
+Begin!
+
+Question: {input}
+Thought:{agent_scratchpad}"""
+)
+
+agent_chain = create_react_agent(
+    llm=llm,
+    tools=tools,
+    prompt=prompt,
+    stop_sequence=False,
+)
+agent_executor = AgentExecutor.from_agent_and_tools(
+    agent=agent_chain,
+    tools=tools,
+    verbose=True,
+    handle_parsing_errors="Invalid format. Either provide an "
+    "Action with Action Input, or a Final Answer only.",
+    max_iterations=3,
+)
+
+print(agent_executor.invoke({"input": "What is the capital of Iran?"}))
+print(agent_executor.invoke({"input": "What is the capital of France?"}))
+print(agent_executor.invoke({"input": "How much is 10+10?"}))
